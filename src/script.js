830d28af22d4f2ebceb6ff4628be5f7b1c3991fb
Rodney brandão <webdeveloperzin@gmail.com>
2025-07-25 04:01:50 -0300

    Initial commit

---
+++ src/script.js
@@ -0,0 +1,5 @@
+let ButTest = document.querySelector('#testar')
+
+ButTest.addEventListener('click', function(){
+    window.open('https://api.whatsapp.com/send?phone=5521996525456&text=Olá,%20quero%20fazer%20um%20teste%20grátis%20do%20iptv%20pfv')
+})
